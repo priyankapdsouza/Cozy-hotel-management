7d787bf7bb6d3302f162ac729ae46978b33728cf
priyankapdsouza <priyankapdsouza7@gmail.com>
2026-02-28 13:01:28 +0530

    Add files via upload

---
+++ Frontend/app/src/app/shared/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  constructor(
+    public auth: AuthService,
+    public cart: CartService
+  ) {}
+}
